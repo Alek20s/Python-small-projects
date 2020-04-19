8513c33a88fa3ef80a2ee49d1d78e6e67b24fd53
Alexei <48793812+Alek20s@users.noreply.github.com>
2020-04-19 19:11:12 +0100

    Add files via upload

---
+++ Sequence of squares/sq.py
@@ -0,0 +1,19 @@
+dsq =[0,1,2,3,4,5,6,7,8,9,10,11,12]
+squares1 =[1,4,9,16,25]
+squares2 =[36,49,64,80,100]
+squares = squares1 +squares2
+squares[8] =9**2
+squares.append(121)
+squares.append(12**2)
+i=1
+dsq[i]= squares[i] - squares[i-1]
+print (squares[:])
+print("alex")
+#print ("dsq (",i,") = ", dsq[i])
+
+
+
+
+
+
+
